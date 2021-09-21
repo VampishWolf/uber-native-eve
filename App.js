1ddf76c8516c118606d0ad0aa9c9206f163f5d14
Ashish Mittal <ashishmittal731@gmail.com>
2021-09-22 01:18:54 +0530

    second iteration

--- App.js
+++ App.js
@@ -1,15 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { store } from './store';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Provider } from 'react-redux';
+import 'react-native-gesture-handler'
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './screens/HomeScreen';
+import MapScreen from './screens/MapScreen';
 
 export default function App() {
+  const Stack = createStackNavigator()
+
   return (
-    <Provider >
-      <View style={styles.container}>
-        <Text>Let's Build!</Text>
-        <StatusBar style="auto" />
-      </View>
+    <Provider store={store}>
+      <NavigationContainer>
+        {/* <SafeAreaProvider> */}
+          <Stack.Navigator>
+              <Stack.Screen name="HomeScreen" component={HomeScreen} options={{
+                headerShown: false,
+              }}/>
+              <Stack.Screen name="MapScreen" component={MapScreen} options={{
+                headerShown: false,
+              }}/>
+          </Stack.Navigator>
+        {/* </SafeAreaProvider> */}
+      </NavigationContainer>
     </Provider>
   );
 }
